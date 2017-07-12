e891bc08948753528066c2d470e2270c4df30062
Mitchel Severe <truth531@gmail.com>
2017-07-12 16:14:24 -0400

    Got car to spawn

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const MAIN_CAR = document.querySelector('#car');
+const OTHER_CAR = document.querySelector('#othercar');
 let carPosition = 100;
+let otherCarPosition = 0;
 
-document.addEventListener('keyup', function(event) {
-    console.log(event.keyCode);
 
-    if (event.keyCode === 39) {
-        carPosition += 300;
-        MAIN_CAR.style.left = `${carPosition}px`;
-    } else if (event.keyCode === 37) {
-        carPosition -= 300;
-        MAIN_CAR.style.left = `${carPosition}px`;
+    document.addEventListener('keyup', function(event) {
+        if (event.keyCode === 39) {
+            if (carPosition < 700) {
+                carPosition += 300;
+                MAIN_CAR.style.left = `${carPosition}px`;
+            }
+        } else if (event.keyCode === 37) {
+            if (carPosition > 100) {
+                carPosition -= 300;
+                MAIN_CAR.style.left = `${carPosition}px`;
+            }
+        }
+    });
+
+    function carSpawn() {
+        OTHER_CAR.style.top = `${otherCarPosition}px`;
+        setTimeout(carSpawn, 10000);
     }
-});
+
+    carSpawn();
