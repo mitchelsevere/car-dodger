979a2544c9f031c16c49e658f1df4129b3811910
Mitchel Severe <truth531@gmail.com>
2017-07-13 09:57:31 -0400

    Other car moves

--- main.js
+++ main.js
@@ -1,35 +1,58 @@
-const MAIN_CAR = document.querySelector('#car');
-const OTHER_CAR = document.querySelector('#othercar');
+let mainCar = document.querySelector('#audi');
+let truck = document.querySelector('#truck');
+let van = document.querySelector('#mini-van');
+let mini = document.querySelector('#mini-truck');
+let sedan = document.querySelector('#car');
+
 let carPosition = 100;
 let otherCarPosition = -200;
-let carSpawnPosition = [100, 400, 700];
-let random = Math.floor(Math.random() * carSpawnPosition.length);
+let carSpawnPositions = [100, 400, 700];
+let otherCars = [truck, van, mini, sedan];
 
+let randomPosition = randomInt(carSpawnPositions);
+let randomCar = randomInt(otherCars);
 
+// Referenced J's div racer logic for the keyup function
+function mainCarMove() {
     document.addEventListener('keyup', function(event) {
         if (event.keyCode === 39) {
             if (carPosition < 700) {
                 carPosition += 300;
-                MAIN_CAR.style.left = `${carPosition}px`;
+                mainCar.style.left = `${carPosition}px`;
             }
         } else if (event.keyCode === 37) {
             if (carPosition > 100) {
                 carPosition -= 300;
-                MAIN_CAR.style.left = `${carPosition}px`;
+                mainCar.style.left = `${carPosition}px`;
             }
         }
     });
+}
 
     function carSpawn() {
-        OTHER_CAR.style.top = `${otherCarPosition}px`;
-        OTHER_CAR.style.left = `${carSpawnPosition[random]}px`;
-        setTimeout(carSpawn, 1000);
+        console.log(randomCar);
+        console.log(randomPosition);
+        currentCar = otherCars[randomCar];
+        currentCar.style.left = `${carSpawnPositions[randomPosition]}px`;
+        currentCar.style.top = `${otherCarPosition}px`;
+        carMove();
     } 
 
+    carSpawn();
+
     function carMove() {
-        carSpawn();
-        OTHER_CAR.style.top = `${otherCarPosition += 100}px`;
-        setTimeout(carMove, 1000);
+        let animate = window.requestAnimationFrame(carMove);
+        if (otherCarPosition > 800) {
+            console.log('hello');
+            otherCarPosition = -200;
+            cancelAnimationFrame(animate);
+        } else {
+            currentCar.style.top = `${otherCarPosition += 5}px`;
+        }
     }
 
-    carMove();
+    function randomInt(arr) {
+       return Math.floor(Math.random() * arr.length);
+    }
+
+    mainCarMove();
